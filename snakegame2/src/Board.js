f60121ae6b7fb9920b685e935ed8605134306e47
Navina K <99734360+navina1@users.noreply.github.com>
2023-07-28 19:17:45 +0530

    boundary condition set

--- snakegame2/src/Board.js
+++ snakegame2/src/Board.js
@@ -40,7 +40,8 @@ function Board() {
         newRows[food.x][food.y] = "food";
         setRows(newRows);
     };
-   
+    const [reset,setReset]=useState(false)
+    
     function useIntervel(callback, delay) {
         const savedCallback = useRef();
         useEffect(() => {
@@ -84,39 +85,62 @@ function Board() {
         return position;
     };
     const [food, setFood] = useState(randomPosition);
+    const [start, setStart] = useState(true);
     const moveSnake = () => {
         const newSnake = [];
         switch (direction) {
             case "right":
-                newSnake.push({ x: snake[0].x, y: (snake[0].y + 1) % width });
+                if (snake[0].y + 1 > 9) {
+                    setStart(false);
+                }
+                    newSnake.push({ x: snake[0].x, y: (snake[0].y + 1)%width });
                 break;
             case "left":
+                if (snake[0].y - 1 < 0) {
+                    setStart(false);
+                }
                 newSnake.push({ x: snake[0].x, y: (snake[0].y - 1 + width) % width });
                 break;
             case "top":
+                if (snake[0].x - 1 < 0) {
+                    setStart(false);
+                }
                 newSnake.push({ x: (snake[0].x - 1 + height) % height, y: snake[0].y });
                 break;
             case "bottom":
+                if (snake[0].x + 1 > 9) {
+                    setStart(false);
+                }
                 newSnake.push({ x: (snake[0].x + 1) % height, y: snake[0].y });
         }
-        snake.forEach((cell) => {
-            newSnake.push(cell);
-        });
-        if (snake[0].x === food.x && snake[0].y === food.y) {
-            setFood(randomPosition);
-            setScore(score+1)
-        } else {
-            newSnake.pop();
+        if (start) {
+            snake.forEach((cell) => {
+                newSnake.push(cell);
+            });
+            if (snake[0].x === food.x && snake[0].y === food.y) {
+                setFood(randomPosition);
+                setScore(score + 1)
+            } else {
+                newSnake.pop();
+            }
+            setSnake(newSnake);
+            displaySnake();
         }
-        setSnake(newSnake);
-        displaySnake();
+        
     };
     useIntervel(moveSnake, 350);
     return (
         <div>
-            {displayRows}
-            <label>Score:{ score}</label>
-    </div> 
+            { <div>
+                {displayRows}
+                <label>Score:{score}</label>
+            </div>
+            }
+            {!start && <div style={{  background: "red", width: "600px", marginLeft: "480px", height: "150px", textAlign: "center" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+                <label style={{ fontSize: "50px", color: "white" }}>Game Over</label>
+                <button style={{ width: "150px", height: "40px", fontSize: "17px", borderRadius: "5px", cursor: "pointer" }} onClick={() => { setScore(0); setStart(true);}}>Restart</button>
+            </div>}
+        </div>
     )
 }
 
